212dfc0572fb29a654c29335ba53238db6e519d8
sanjeev <sanjeevkdk32@gmail.com>
2017-12-20 17:03:00 +0545

    multiple file upload

--- resources/views/includes/backview/footer.blade.php
+++ resources/views/includes/backview/footer.blade.php
@@ -1,3 +1,21 @@
 <script src="{{ asset('js/app.js') }}"></script>
+<script type="text/javascript">
+
+	var form = document.getElementById('upload');
+	var request = new  XMLHttpRequest();
+
+	form.addEventListener('submit', function(){
+        e.prreventDefault();
+        var formdata = new FormData(form);
+
+        request.open('post', 'upload');
+        request.addEventListener("load", transferComplete);
+        request.send(formdata);
+	});
+
+	function transferComplete(data){
+        console.log(data.currentTarget.response);
+	}	
+</script>
 </body>
 </html>
